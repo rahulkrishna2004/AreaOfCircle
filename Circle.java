59f13d692c180a6324b5b0c1d8df7f1680076187
rahulkrishna2004 <110278263+rahulkrishna2004@users.noreply.github.com>
2025-03-03 15:46:24 +0530

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,7 @@
+public class Circle {
+    public static void main(String[] args) {
+        double radius = 5.0; // Static radius value
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of the circle is: " + area);
+    }
+}
